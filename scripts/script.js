7e866d1a0abf6283685ae4a553a2c0bb05510bbf
coreuser <david.mcmahon@corehr.com>
2017-09-11 09:56:26 +0100

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+console.log("Hey")
+console.log("A change")
+console.log("Another change")
+
+for(var i = 0 ; i < 3 ; i++) {
+	console.log("Index: " + i);
+}
+
+console.log("New change to trigger anther build")
